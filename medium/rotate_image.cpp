d8487152f983ced66487c6bd1a86fcdc4ad88222
Ayush Mehta <ayushmehtabps@gmail.com>
2026-06-03 11:04:59 +0530

    rotate image 90 degree

---
+++ medium/rotate_image.cpp
@@ -0,0 +1,45 @@
+//rotate 3x3 matrix by 90*
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+void rotate(vector<vector<int>>&matrix){
+
+
+    // tarnspose the matrix first 
+    // interchanging the rows and cols
+
+    int n=matrix.size();
+
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+
+    //reverse the rows 
+    for(int i=0;i<n;i++){
+         reverse(matrix[i].begin(),matrix[i].end());
+    }
+}
+
+
+int main(){
+
+    vector<vector<int>>matrix={{1,2,3},{4,5,6},{7,8,9}};
+
+    rotate(matrix);
+
+
+    for(int i=0;i<matrix.size();i++){
+        cout<<endl;
+        for(auto it:matrix[i]){
+            cout<<it<<" ";
+        }
+    }
+
+}
