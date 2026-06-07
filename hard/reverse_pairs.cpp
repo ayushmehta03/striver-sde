20d21b67342f51f740a1427d32cb5f39ab8b8dd1
Ayush Mehta <ayushmehtabps@gmail.com>
2026-06-07 10:01:59 +0530

    add reverse_pair optimal solution

--- hard/reverse_pairs.cpp
+++ hard/reverse_pairs.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //sc O(1)
 
 
-int revPairs(vector<int>&nums){
+/*int revPairs(vector<int>&nums){
 
     int n=nums.size();
 
@@ -25,7 +25,81 @@ int revPairs(vector<int>&nums){
     return cnt;
 
 
+} */
+
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
 }
+
+
+
+
+int countPairs(vector<int> &arr, int low, int mid, int high) {
+    int right = mid + 1;
+    int cnt = 0;
+    for (int i = low; i <= mid; i++) {
+        while (right <= high && arr[i] > 2 *(long long) arr[right]) right++;
+        cnt += (right - (mid + 1));
+    }
+    return cnt;
+}
+
+int mergeSort(vector<int>&arr,int low,int high){
+
+    int cnt=0;
+
+    if(low>=high) return cnt;
+
+    int mid=(low+high)/2;
+    cnt += mergeSort(arr, low, mid);  // left half
+    cnt += mergeSort(arr, mid + 1, high); // right half
+    cnt += countPairs(arr, low, mid, high); //Modification
+    merge(arr, low, mid, high);  // merging sorted halves
+    return cnt;
+
+}
+
+
+int reversePair(vector<int>&nums){
+    int n=nums.size();
+    return mergeSort(nums,0,n-1);
+
+}
+
+
 
 int main(){
 
@@ -37,7 +111,10 @@ int main(){
 
     for(int i=0;i<n;i++)cin>>nums[i];
 
-    cout<<revPairs(nums);
+   cout<<reversePair(nums);
+
+
+
 
     return 0;
 
