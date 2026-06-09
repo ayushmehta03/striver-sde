f51ed606d67a89581a700ae943973f45d06283c0
Ayush Mehta <ayushmehtabps@gmail.com>
2026-06-09 10:14:42 +0530

    add longest_subarray len soln

---
+++ medium/longest_subarray.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+
+
+
+//appraoch 1 using hahmap to store sum ad indices
+
+
+int longestSubA(vector<int>&nums,int k){
+
+    int n=nums.size();
+
+    unordered_map<int,int>mpp;
+
+    int sum=0;
+
+    int maxLen=0;
+
+    for(int i=0;i<n;i++){
+     sum+=nums[i];
+
+     if(sum==k){
+        maxLen=max(maxLen,i+1);
+     }
+
+     int rem=sum-k;
+
+     if(mpp.find(rem)!=mpp.end()){
+        int len=i-mpp[rem];
+        maxLen=max(maxLen,len);
+     }
+
+
+     if(mpp.find(sum)==mpp.end()){
+        mpp[sum]=i;
+     }
+
+    }
+
+    return maxLen;
+
+}
+
+
+
+int main(){
+
+
+    vector<int>nums={1,2,3,1,1,1,1,4,2,3};
+
+    int k=3;
+
+
+    cout<<"the length of longest suba with sum k is: "<<longestSubA(nums,k);
+
+    return 0;
+
+
+
+}
